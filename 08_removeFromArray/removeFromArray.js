b69fb829b1933ff355559c635eb3cc27836ede29
Duy Le <94324394+SabreBirdOne@users.noreply.github.com>
2025-08-01 13:02:26 -0400

    Fix removeFromArray.js in exercise 8
    I love functional programming in JavaScript.
    One-liners all day long.

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,6 @@
-const removeFromArray = function() {
+// Returns a new array made from arr with values in the others rest parameter reomved.  
+const removeFromArray = function(arr, ...others) {
+    return arr.filter((item) => !others.includes(item));
 };
 
 // Do not edit below this line
