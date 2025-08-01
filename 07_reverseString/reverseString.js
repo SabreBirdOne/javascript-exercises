82a5e9e196209fdc66caeb6f30262179dc7d7deb
Duy Le <94324394+SabreBirdOne@users.noreply.github.com>
2025-08-01 12:46:48 -0400

    Fix reverseString.js in exercise 7

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,6 @@
-const reverseString = function() {
-
+// Returns the str with characters in reverse order.
+const reverseString = function(str) {
+    return str.split("").reverse().join("");
 };
 
 // Do not edit below this line
