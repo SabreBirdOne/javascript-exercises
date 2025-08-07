a87362e0e2724b6edac72d3fb8b8f402039fedcd
Duy Le <94324394+SabreBirdOne@users.noreply.github.com>
2025-08-07 15:53:58 -0400

    Fix findTheOldest.js in exercise 16

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,13 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    return people.reduce((oldest, current) => {
+        if (current.yearOfDeath === undefined || oldest.yearOfDeath === undefined) {
+            return oldest.yearOfBirth < current.yearOfBirth ? oldest : current;
+        }
+        if (oldest.yearOfDeath - oldest.yearOfBirth < current.yearOfDeath - current.yearOfBirth){
+            return current;
+        }
+        return oldest;
+    });
 };
 
 // Do not edit below this line
