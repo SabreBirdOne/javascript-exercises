b5685197b8078a1c40badee5d877db0d2a445973
Duy Le <94324394+SabreBirdOne@users.noreply.github.com>
2025-08-07 15:10:24 -0400

    Fix palindromes.js in exercise 13
    String ops in Javascript is annoying.
    My solution may be better than the given
    solution, since I use less function calls.

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    const alphanumerical = 'abcdefghijklmnopqrstuvwxyz0123456789';
 
+    let newstr = str
+        .toLowerCase()
+        .split('')
+        .filter((char) => alphanumerical.includes(char))
+    
+    return newstr.join('') === newstr.reverse().join('');
 };
 
 // Do not edit below this line
