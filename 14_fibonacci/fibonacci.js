28b5db0e6cd458cb15e2ed0250b6bac2e60614e1
Duy Le <94324394+SabreBirdOne@users.noreply.github.com>
2025-08-07 15:34:33 -0400

    Fix fibonacci.js in exercise 14

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    n = +n;
+    if (n < 0) return "OOPS";
+    
+    let firstPrev = 1;
+    let secondPrev = 0;
+    
+    if (n === 0) return secondPrev;
 
+    for (let i = 2; i <= n; i++) {
+        let current = firstPrev + secondPrev;
+        secondPrev = firstPrev;
+        firstPrev = current;
+    }
+
+    return firstPrev;
 };
 
 // Do not edit below this line
