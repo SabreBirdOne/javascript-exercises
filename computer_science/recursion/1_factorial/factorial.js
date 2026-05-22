e764277191289819bd36d2e0d9ac29a4596101b9
Duy Le <94324394+SabreBirdOne@users.noreply.github.com>
2026-05-22 17:25:30 -0400

    Solve factorial exercise in recursion

--- computer_science/recursion/1_factorial/factorial.js
+++ computer_science/recursion/1_factorial/factorial.js
@@ -1,5 +1,8 @@
-const factorial = function() {
-
+const factorial = function(n) {
+    if (Number.isInteger(n) && n >= 0){
+        return (n === 1 || n === 0) ? 1 : n * factorial(n - 1);
+    }
+    return undefined;
 };
 
 // Do not edit below this line
