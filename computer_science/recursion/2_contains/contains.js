e6e57ab24eea7676fcfae1c0951ef375179e4a37
Duy Le <94324394+SabreBirdOne@users.noreply.github.com>
2026-05-22 18:07:17 -0400

    Solve contains in recursion

--- computer_science/recursion/2_contains/contains.js
+++ computer_science/recursion/2_contains/contains.js
@@ -1,5 +1,19 @@
-const contains = function() {
-  
+const contains = function(thing, target) {
+    console.log(thing, typeof thing);
+    if (thing !== Object(thing)){
+        if (Number.isNaN(target)){
+            return Number.isNaN(thing);
+        }
+        return thing === target;
+    }
+    
+    if (typeof thing === 'object'){
+        if (thing === target) return true;
+        for (const item of Object.values(thing)){
+            if (contains(item, target)) return true;
+        }
+        return false;
+    }
 };
   
 // Do not edit below this line
