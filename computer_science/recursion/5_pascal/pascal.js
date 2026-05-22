559b5611ed4bd1f1bd87c0929299e22d3ae001ae
Duy Le <94324394+SabreBirdOne@users.noreply.github.com>
2026-05-22 19:20:06 -0400

    Solve pascal in recursion

--- computer_science/recursion/5_pascal/pascal.js
+++ computer_science/recursion/5_pascal/pascal.js
@@ -1,5 +1,18 @@
-const pascal = function() {
+const pascal = function(n) {
+  if (n === 0 || n === 1) return [1];
   
+  let arr = [];
+
+  const template = [0].concat(pascal(n - 1)).concat([0]);
+
+  // from second element of template to the end, 
+  // push the sum of current and previous element to arr
+
+  for (let i = 1; i < template.length; i++) {
+    arr.push(template[i - 1] + template[i]);
+  }
+
+  return arr;
 };
   
 // Do not edit below this line
