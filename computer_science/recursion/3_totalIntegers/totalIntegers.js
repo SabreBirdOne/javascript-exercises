f8185fa257b1d6f8463d7f088ffacc0eff2b1f9d
Duy Le <94324394+SabreBirdOne@users.noreply.github.com>
2026-05-22 18:43:36 -0400

    Solve totalIntegers in recursion

--- computer_science/recursion/3_totalIntegers/totalIntegers.js
+++ computer_science/recursion/3_totalIntegers/totalIntegers.js
@@ -1,5 +1,18 @@
-const totalIntegers = function() {
+const totalIntegers = function(object) {
   
+  if (Array.isArray(object)){
+    return object.reduce((sum, element) => {
+        if (Number.isInteger(element)) sum += 1;
+        else sum += (totalIntegers(element) || 0);
+        return sum;
+    }, 0)
+  }
+  else if (typeof object === 'object' && object) {
+    return totalIntegers(Object.values(object));
+  }
+  else {
+    return undefined;
+  }
 };
   
 // Do not edit below this line
