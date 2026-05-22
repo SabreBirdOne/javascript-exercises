6bd886476230bb2243b7b5acb574421496ad09d7
Duy Le <94324394+SabreBirdOne@users.noreply.github.com>
2026-05-22 19:01:09 -0400

    Solve permutuations in recursion

--- computer_science/recursion/4_permutations/permutations.js
+++ computer_science/recursion/4_permutations/permutations.js
@@ -1,5 +1,18 @@
-const permutations = function() {
-  
+const permutations = function(arr) {
+    if (arr.length === 0 || arr.length === 1) return [arr];
+    
+    let perms = [];
+
+    for (let i = 0; i < arr.length; i++){
+        // Get permutations of remaining elements in arr
+        const remaining = arr.toSpliced(i, 1);
+        const remaining_perms = permutations(remaining);
+
+        // Glue this element onto every permutation as resulting new_perm
+        remaining_perms.forEach((rem_perm) => perms.push([arr[i]].concat(rem_perm)))
+    }
+
+    return perms;
 };
   
 // Do not edit below this line
